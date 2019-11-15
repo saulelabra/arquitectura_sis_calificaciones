287edefcf039b3b294505271f002b71b5721ce25
saulelabra <quiquelabra@gmail.com>
2019-11-15 17:55:20 -0600

    avance

---
+++ sistema-calificaciones/frontend/src/classes/Student.js
@@ -0,0 +1,10 @@
+class Strudent {
+    constructor(matricula, nombre, cAcad, cEq, cCom, estado) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.cAcad = cAcad;
+        this.cEq = cEq;
+        this.cCom = cCom;
+        this.estado = estado;
+    }
+}
---
+++ sistema-calificaciones/frontend/src/reducers/rootReducer.js
@@ -0,0 +1,25 @@
+/*import Student from '../classes/Student';
+
+var studentArr = [];
+
+userArr.push(new User("Saúl Enrique", "Labra", "1234", "quique"));
+userArr.push(new User("Rodrigo", "Garcia", "5678"));
+userArr.push(new User("Manuel", "Guadarrama", "abcd"));
+userArr.push(new User("Emilio", "Hernandez", "efghi"));
+
+fileArr.push(new File(0, "imagen1", ".jpg", "24/10/2019"));
+fileArr.push(new File(1, "Texto prueba", ".txt", "24/10/2019"));
+fileArr.push(new File(2, "Documento prueba", ".docx", "24/10/2019"));
+fileArr.push(new File(3, "imagen2", ".png", "24/10/2019"));
+
+var data = new Data(fileArr, userArr, countArr);
+
+function rootReducer(state = data, {type, payload}) {
+    switch(type) {
+        default:
+            console.log('entró al default');
+            return state;
+    }
+}
+
+export default rootReducer;*/
